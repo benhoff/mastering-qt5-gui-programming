3b61788748a1d747727c9345d974729cfd4ec1ca
Ben Hoff <hoff.benjamin.k@gmail.com>
2018-02-11 12:44:41 -0700

    comments

--- 2_section/2_filtering/photomodel.h
+++ 2_section/2_filtering/photomodel.h
@@ -18,13 +18,15 @@ public:
         // NOTE: This block of text loads in our pictures
         for (int i = 0; i < 100; i++)
         {
-            // Dynamically generate our filepath
+            // Create Icon
             QString filepath(":/pics/" + QString::number(i) + ".png");
-            // Create a new QStandaradItem
             QIcon icon(filepath);
+
+            // Create Item & Set Icon
             QStandardItem *item = new QStandardItem();
             item->setIcon(icon);
-            // set the data for the new QStandard item to be the image that we just loaded
+
+            // Set Item in model
             this->setItem(i, 0, item);
         }
 
