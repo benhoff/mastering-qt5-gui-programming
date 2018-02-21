6884b1265b1cd7880581e7d2dee32243d01901e7
Ben Hoff <hoff.benjamin.k@gmail.com>
2018-02-21 16:19:52 -0700

    added in beginning code

--- 2_section/4_tree/BEGINNING_CODE/picturemodel.h
+++ 2_section/4_tree/BEGINNING_CODE/picturemodel.h
@@ -2,6 +2,8 @@
 #define PICTUREMODEL_H 
 
 #include <QAbstractItemModel>
+#include "huegenerator.h"
+#include "treeitem.h"
 
 class PictureModel: public QAbstractItemModel
 {
@@ -9,21 +11,24 @@ class PictureModel: public QAbstractItemModel
 
 public:
     explicit PictureModel(QObject *parent = nullptr);
+    ~PictureModel();
 
-    // Header:
-    QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const override;
-
-    // Basic functionality:
     QModelIndex index(int row, int column,
                       const QModelIndex &parent = QModelIndex()) const override;
     QModelIndex parent(const QModelIndex &index) const override;
 
-    int rowCount(const QModelIndex &parent = QModelIndex()) const override;
-    int columnCount(const QModelIndex &parent = QModelIndex()) const override;
+    int rowCount(const QModelIndex &parent=QModelIndex()) const override;
+    int columnCount(const QModelIndex &parent=QModelIndex()) const override;
 
     QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
+    Qt::ItemFlags flags(const QModelIndex &index) const override;
+
+    void add_colors(ColorVec colors);
 
 private:
+    ColorTreeItem* get_item_from_index(const QModelIndex &index) const;
+
+    ColorTreeItem *root_item;
 };
 
 #endif // PICTUREMODEL_H 
