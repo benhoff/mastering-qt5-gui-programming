45479bc49effac99ab09c0c62d76305ab5d05805
Ben Hoff <hoff.benjamin.k@gmail.com>
2018-02-23 05:47:50 -0700

    getting ready to start producing

---
+++ 1_section/2_eventloop_graphicless/FINAL_CODE/processmanager.h
@@ -0,0 +1,31 @@
+#ifndef PROCESSMANAGER_H
+#define PROCESSMANAGER_H
+
+#include <QObject>
+#include <QVector>
+#include <QProcess>
+#include <QTimer>
+
+class ProcessManager : public QObject
+{
+    Q_OBJECT
+public:
+    explicit ProcessManager(int max_processes=5, QObject *parent = nullptr);
+    void start();
+
+signals:
+
+private slots:
+    void start_process();
+    void process_finished(int exit_code, QProcess::ExitStatus status);
+    void error_handler(QProcess::ProcessError error);
+
+
+private:
+    QTimer *_timer;
+    int _num_process;
+    int _max_processes;
+    QVector<QProcess*> _processes;
+};
+
+#endif // PROCESSMANAGER_H
