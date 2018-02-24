b3c37eacee1905efe7a3a9dc33c1ff5cbb03a574
Ben Hoff <hoff.benjamin.k@gmail.com>
2018-02-24 13:35:04 -0700

    added in final code

--- 1_section/2_eventloop_graphicless/FINAL_CODE/processmanager.cpp
+++ 1_section/2_eventloop_graphicless/FINAL_CODE/processmanager.cpp
@@ -1,68 +1,94 @@
 #include "processmanager.h"
 #include <QDebug>
 
+
 ProcessManager::ProcessManager(int max_processes, QObject *parent) :
     QObject(parent),
     _num_process(0),
-    _max_processes(max_processes)
+    _max_processes(max_processes),
+    _sacrifice(new QProcess(this))
 {
-    _timer = new QTimer(this);
+    _timer = new QTimer();
     _timer->setInterval(1500);
 
-    connect(_timer, &QTimer::timeout, this, &ProcessManager::start_process);
+    connect(_timer, &QTimer::timeout, this, QOverload<>::of(&ProcessManager::start_process));
+
+    // simulates a kill process signal from the network
+    QTimer::singleShot(8000, [this](){kill_process(_sacrifice, "Sacrifical Process");});
 }
 
-void ProcessManager::start()
+void ProcessManager::start_process(QString name)
 {
-    qDebug() << "starting timer";
-    _timer->start();
-}
-
-void ProcessManager::start_process()
-{
-    qDebug() << "starting process # " << _num_process;
+    qDebug() << "starting process # " << _num_process << " process: " << name;
 
     QProcess *process = new QProcess(this);
     _processes.append(process);
+    process->start(name);
 
-    // NOTE: put your binary here!
-    process->start("ls");
-
-    // process->setProcessEnvironment();
-
-    connect(process,
-                  SIGNAL(finished(int, QProcess::ExitStatus)),
-                  this,
-                  SLOT(process_finished(int, QProcess::ExitStatus)));
-
-    connect(process,
-            SIGNAL(errorOccurred(QProcess::ProcessError)),
-            this,
-            SLOT(error_handler(QProcess::ProcessError)));
-
-    // TODO: ready read standard output?
+    setup_connections(process, name);
 
     _num_process += 1;
 
     if (_num_process == _max_processes)
     {
-        qDebug() << "max processes reached!";
+        qDebug() << "max processes reached! Stopping timer!";
         _timer->stop();
     }
 }
 
-void ProcessManager::process_finished(int exit_code, QProcess::ExitStatus status)
+void ProcessManager::setup_connections(QProcess *process, QString process_name)
+{
+    // TODO: Connect the `finished` and `errorOccured` signals up
+    connect(process, QOverload<int, QProcess::ExitStatus>::of(&QProcess::finished),
+            [process, process_name, this](int exit_code, QProcess::ExitStatus status){this->process_finished(exit_code, status, process, process_name);});
+
+    connect(process, &QProcess::errorOccurred, [process, process_name, this](QProcess::ProcessError error){this->error_handler(error, process, process_name);});
+
+
+
+    // NOTE: Unused process signals:
+    //       started, stateChanged, readyReadStandard[Error/Output]
+
+}
+
+void ProcessManager::process_finished(int exit_code, QProcess::ExitStatus status, QProcess* process, QString process_name)
 {
     if (status == QProcess::NormalExit)
-        qDebug() << "        Process finished! " << exit_code << " " << status;
+    {
+        qDebug() << "normal exit for process: " << process_name;
+        _processes.removeOne(process);
+    }
     else if (status == QProcess::CrashExit)
     {
+        qWarning() << "Process crashed! Process: " << process_name;
+        qDebug() << "Exit code: " << exit_code;
+        process->start(process_name);
     }
 }
 
-void ProcessManager::error_handler(QProcess::ProcessError error)
+void ProcessManager::kill_process(QProcess* process, QString name)
+{
+    qDebug() << "Killed process: " << name;
+
+#ifdef W_OS_WIN
+    // Console applications on Windows that do not run an event loop,
+    // or whose event loop does not handle the WM_CLOSE message, can only be terminated by calling kill
+    process->kill();
+#else
+    // Nice version
+    process->terminate();
+    // process->kill();
+#endif
+
+    // Not nice version
+
+    return;
+}
+
+void ProcessManager::error_handler(QProcess::ProcessError error, QProcess *process, QString process_name)
 {
     qDebug() << "Error!";
+
     switch (error) {
     case QProcess::Crashed:
 
@@ -88,4 +114,16 @@ void ProcessManager::error_handler(QProcess::ProcessError error)
 
         break;
     }
+}
+
+void ProcessManager::start()
+{
+    qDebug() << "Starting!";
+    _timer->start();
+    _sacrifice->start();
+}
+
+void ProcessManager::start_process()
+{
+    start_process(QString());
 }
--- 1_section/2_eventloop_graphicless/FINAL_CODE/processmanager.h
+++ 1_section/2_eventloop_graphicless/FINAL_CODE/processmanager.h
@@ -17,15 +17,20 @@ signals:
 
 private slots:
     void start_process();
-    void process_finished(int exit_code, QProcess::ExitStatus status);
-    void error_handler(QProcess::ProcessError error);
-
+    void start_process(QString name);
+    void process_finished(int exit_code, QProcess::ExitStatus status, QProcess *process, QString process_name);
+    void error_handler(QProcess::ProcessError error, QProcess *process, QString process_name);
+    void kill_process(QProcess* process, QString name);
 
 private:
+    void setup_connections(QProcess *process, QString process_name);
+
     QTimer *_timer;
     int _num_process;
     int _max_processes;
     QVector<QProcess*> _processes;
+
+    QProcess *_sacrifice;
 };
 
 #endif // PROCESSMANAGER_H
