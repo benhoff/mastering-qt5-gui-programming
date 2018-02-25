b717db0c37ab89c011a82ac46972688699366ae4
Ben Hoff <hoff.benjamin.k@gmail.com>
2018-02-25 13:25:01 -0700

    cleaned up code

--- 1_section/3_supercomputing/push_server.h
+++ 1_section/3_supercomputing/push_server.h
@@ -4,50 +4,21 @@
 #include <iostream>
 #include <QStringList>
 
+#include <QTimer>
+#include <QDebug>
 #include <QTcpServer>
 #include <QDataStream>
 #include <QTcpSocket>
-#include <QTimer>
 
 
 class PushServer : public QObject
 {
     Q_OBJECT
 public:
-    explicit PushServer(int number_operations = 10,  QObject *parent=nullptr)
-        : QObject(parent)
-    {
-        std::cout << "Start Push server" << std::endl;
-        started = _server.listen(QHostAddress::LocalHost, 5000);
-
-        for (int i = 0; i < number_operations; i++)
-            _operations << QString::number(i);
-
-        connect(&_server, &QTcpServer::newConnection, this, &PushServer::send_work);
-    }
+    explicit PushServer(int number_operations = 10,  QObject *parent=nullptr);
 
 private slots:
-    void send_work()
-    {
-        QTcpSocket *client_connection = _server.nextPendingConnection();
-        connect(client_connection, &QAbstractSocket::disconnected, client_connection, &QObject::deleteLater);
-
-        QByteArray block;
-        QDataStream out(&block, QIODevice::WriteOnly);
-
-
-        QString operation = _operations.takeFirst();
-
-        out << operation;
-        std::cout << "send work: " << operation.toInt() << std::endl;
-
-        client_connection->write(block);
-        client_connection->disconnectFromHost();
-
-        if (_operations.isEmpty())
-            // Need to give the client connection a second to write the last instruction as `write` is non-blocking
-            QTimer::singleShot(50, [this](){emit finished();});
-    }
+    void send_work();
 
 signals:
     void finished();
