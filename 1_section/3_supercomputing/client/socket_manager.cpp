360cff7c0cec89714d1adaa1bdfa2cc2497f417c
Ben Hoff <hoff.benjamin.k@gmail.com>
2018-03-19 23:51:36 -0600

    examples build now

--- 1_section/3_supercomputing/client/socket_manager.cpp
+++ 1_section/3_supercomputing/client/socket_manager.cpp
@@ -18,48 +18,23 @@ void SocketManager::setup_sockets()
 {
     for (int sock_num =0; sock_num < _total_sockets; sock_num++)
     {
-        SocketPair socket_pair();
+        SocketPair socket_pair;
         _sockets.append(socket_pair);
+        _setup_error_handling(socket_pair, sock_num);
 
-        socket_pair.output->connect(QHostAddress::LocalHost, 5001);
+        socket_pair.output->connectToHost(QHostAddress::LocalHost, 5001);
 
         QDataStream *data_stream = new QDataStream();
         _data_streams.append(data_stream);
 
         data_stream->setDevice(socket_pair.input);
 
-        connect(socket_pair.input, &QIODevice::readyRead, [this, sock_num, in](){
+        connect(socket_pair.input, &QIODevice::readyRead, [this, sock_num, data_stream, socket_pair](){
 
         });
     }
-
 }
 
-void SocketManager::start()
-{
-    std::cout << "Start" << std::endl << std::endl;
-    _elasped_time.start();
-    for (int i = 0; i < _total_sockets; i++)
-    {
-        QTcpSocket *socket = _sockets[i];
-        socket->connectToHost(QHostAddress::LocalHost, 5000);
-    }
-}
-
-void SocketManager::setup_socket_connections(QTcpSocket *socket, QDataStream *data_stream, int socket_number)
-{
-    connect(socket, &QIODevice::readyRead, [](){});
-    connect(socket,
-            QOverload<QAbstractSocket::SocketError>::of(&QAbstractSocket::error), [](){});
-}
-
-
-void SocketManager::get_more_work(int socket_number)
-{
-    QTcpSocket *socket = _sockets[socket_number].input;
-    socket->abort();
-    socket->connectToHost(QHostAddress::LocalHost, 5000);
-}
 
 void SocketManager::do_work(QString work, SocketPair socket_pair, int socket_number)
 {
@@ -78,32 +53,48 @@ void SocketManager::do_work(QString work, SocketPair socket_pair, int socket_num
     std::cout << "Did work: " << work.toStdString() << " with socket # " << socket_number << std::endl;
 
 
-    // int simulated_work_time = _random.bounded(500);
-    // QTimer::singleShot(simulated_work_time, [this, sock_num](){get_more_work(sock_num);});
-    QTimer::singleShot(simulated_work_time, [this, block, socket_number](){
-        QTcpSocket *socket = _sockets[socket_number + _total_sockets];
-    });
+    int simulated_work_time = _random.bounded(500);
+    QTimer::singleShot(simulated_work_time, [this, socket_number](){get_more_work(socket_number);});
 
-    if (work.toInt() == 100)
+    if (work.toInt() == 101)
     {
         std::cout << "Time elasped: " << (float) _elasped_time.elapsed() / 1000 << " seconds" << std::endl;
         emit finished();
     }
 }
 
+
+void SocketManager::start()
+{
+    std::cout << "Start" << std::endl << std::endl;
+    _elasped_time.start();
+    for (int i = 0; i < _total_sockets; i++)
+    {
+        SocketPair socket_pair = _sockets[i];
+        socket_pair.input->connectToHost(QHostAddress::LocalHost, 5000);
+    }
+}
+
+void SocketManager::get_more_work(int socket_number)
+{
+    QTcpSocket *socket = _sockets[socket_number].input;
+    socket->abort();
+    socket->connectToHost(QHostAddress::LocalHost, 5000);
+}
+
 void SocketManager::error_handler(QAbstractSocket::SocketError error, int socket_number)
 {
     if (error == QAbstractSocket::ConnectionRefusedError)
     {
-        QTimer::singleShot(1000, [this, socket_number](){reset_socket(socket_number);});
+        QTimer::singleShot(1000, [this, socket_number](){get_more_work(socket_number);});
         if (_random.bounded(_total_sockets) == 0)
             std::cout << "No host found, is the server running?" << std::endl;
     }
 }
 
-void SocketManager::_setup_error_handling(QTcpSocket* socket)
+void SocketManager::_setup_error_handling(SocketPair socket_pair, int socket_number)
 {
-    connect(socket,
-            QOverload<QAbstractSocket::SocketError>::of(&QAbstractSocket::error),
-            [this, sock_num](QAbstractSocket::SocketError error){error_handler(error, sock_num);});
+    connect(socket_pair.input,
+                  QOverload<QAbstractSocket::SocketError>::of(&QAbstractSocket::error),
+                  [this, socket_number](QAbstractSocket::SocketError error){error_handler(error, socket_number);});
 }
--- 1_section/3_supercomputing/client/socket_manager.h
+++ 1_section/3_supercomputing/client/socket_manager.h
@@ -11,14 +11,14 @@
 class SocketPair
 {
 public:
-    explict SocketPair()
+    explicit SocketPair()
     {
         input = new QTcpSocket();
         output = new QTcpSocket();
     }
     QTcpSocket *input;
     QTcpSocket *output;
-}
+};
 
 class SocketManager : public QObject
 {
@@ -37,8 +37,7 @@ private slots:
 
 private:
     void setup_sockets();
-    void setup_socket_connections(QTcpSocket* socket, QDataStream* data_stream, int socket_number);
-    void _setup_error_handling(QTcpSocket *socket);
+    void _setup_error_handling(SocketPair socket_pair, int socket_number);
 
     void get_more_work(int socket_number);
     void do_work(QString work, SocketPair socket_pair, int socket_number);
