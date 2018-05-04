683eafa8ee871b6fa572c4bde575a340bc7e8e4b
Ben Hoff <hoff.benjamin.k@gmail.com>
2018-05-03 20:40:34 -0600

    tracking inital config

---
+++ 5_qt_communication/3_qml_types/BEGINNING_CODE/main.cpp
@@ -0,0 +1,33 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include <QQmlContext>
+#include "photomodel.h"
+#include "colorpicker.h"
+
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+
+    // NOTE: we use `QGuiApplication` instead of `QApplication` as
+    // we're not using widgets
+    QGuiApplication app(argc, argv);
+
+    qmlRegisterType<ColorPicker>("ColorPickers", 1, 0, "ColorPicker");
+
+    QQmlApplicationEngine engine;
+
+
+
+    PhotoModel photo_model;
+
+    QQmlContext *root_context = engine.rootContext();
+    root_context->setContextProperty("photo_model", &photo_model);
+
+    engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
+
+    if (engine.rootObjects().isEmpty())
+        return -1;
+
+    return app.exec();
+}
