25dab433c78f1a0990741bea0d6699f3e3f5c4cb
Ben Hoff <hoff.benjamin.k@gmail.com>
2018-05-08 07:25:48 -0600

    wip

--- 6_visual/1_charts/main.cpp
+++ 6_visual/1_charts/main.cpp
@@ -1,5 +1,31 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QAudioFormat>
+#include <QAudioDecoder>
+
+
+void get_thing()
+{
+    QAudioFormat format;
+    format.setChannelCount(2);
+    format.setCodec("audio/x-raw");
+    format.setSampleType(QAudioFormat::UnSignedInt);
+    format.setSampleRate(48000);
+    format.setSampleSize(16);
+
+    QAudioDecoder *audio_decoder = new QAudioDecoder();
+    audio_decoder->setAudioFormat(format);
+    // FIXME
+    // audio_decoder->setSourceFilename("/my/filename/here.wav");
+    // FIXME
+    // connect(audio_decoder, &QAudioDecoder::bufferReady, this, buffer_slot);
+
+    // audio_decoder->start();
+    QAudioBuffer my_buffer = audio_decoder->read();
+
+    QAudioBuffer result_buffer;
+
+}
 
 int main(int argc, char *argv[])
 {
--- 6_visual/1_charts/main.qml
+++ 6_visual/1_charts/main.qml
@@ -1,9 +1,29 @@
 import QtQuick 2.9
 import QtQuick.Window 2.2
+import QtCharts 2.0
+import QtQuick.Layouts 1.3
+
+// https://doc.qt.io/qt-5.10/qtdatavisualization-qmlmultigraph-example.html
 
 Window {
     visible: true
     width: 640
     height: 480
     title: qsTr("Hello World")
+    GridLayout {
+        id: grid_layout
+        columns: 2
+        Layout.fillHeight: true
+        Layout.fillWidth: true
+        anchors.fill: parent
+
+        Rectangle {
+            Layout.fillHeight: true
+            Layout.fillWidth: true
+            border.color: surface_graph.theme.gridLineColor
+            border.width: 2
+
+        }
+
+    }
 }
